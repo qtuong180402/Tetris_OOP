1d7c39affa2a99441ab13a866dd97d48fbbdbd15
qtuong180402 <113403660+qtuong180402@users.noreply.github.com>
2024-12-18 11:21:08 +0700

    Add files via upload

---
+++ OOPojectlab_ver1/src/Testapp1/Application.java
@@ -0,0 +1,27 @@
+package Testapp1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Hải Đoàn
+ */
+public class Application {
+
+    Tetris tetris;
+
+    public Application() {
+        tetris = new Tetris(this);
+        tetris.setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        new Application();
+    }
+
+}
